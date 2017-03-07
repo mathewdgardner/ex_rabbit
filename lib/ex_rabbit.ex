2bf335e52b2e74519f3ec4ac771630c25b0bb474
Mathew Gardner <mathew@vin.li>
2017-03-07 15:20:22 -0600

    Fix callback typespecs.

--- lib/ex_rabbit.ex
+++ lib/ex_rabbit.ex
@@ -1,6 +1,6 @@
 defmodule ExRabbit do
-  @callback config() :: {:ok, List.t} | {:error, String.t}
-  @callback handler(Map, Map) :: {:ok, List.t} | {:error, String.t}
+  @callback config() :: {:ok, list()} | {:error, String.t}
+  @callback handler(Map, Map) :: {:ok, list()} | {:error, String.t}
 
   @spec publish(String.t, String.t, map() | String.t, keyword()) :: :ok
   def publish(exchange, routing_key, payload, options \\ [])
